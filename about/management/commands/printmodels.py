7969307fe58266c49d32b682d511b8485ae33fef
Dmitry Utkin <dmitry.utkin@gmail.com>
2009-10-19 21:22:29 +0300

    fixed printmodels with loading.get_models()

--- about/management/commands/printmodels.py
+++ about/management/commands/printmodels.py
@@ -1,18 +1,16 @@
 # -*- coding: utf-8 -*-
 
-import inspect
-import django.db.models
+#import inspect
+#import django.db.models
+from django.db.models.loading import get_models
 from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):
     'printmodels django management command'
     def handle(self, *args, **options):
         "iterate over app's models and print their names and count in db"
-        for mod in django.db.models.get_apps():#get all installed apps' models
-            for name in dir(mod):
-                m = getattr(mod, name)#convert names to modules
-                #if module is a class and is a django model - count and print
-                if inspect.isclass(m) and issubclass(m, django.db.models.Model):
-                    count = m.objects.count()
-                    print m.__module__+'.'+name, count
+        for model in get_models():
+            print '%s.%s %d' % \
+                (model.__module__, model.__name__, model.objects.count())
+
 
